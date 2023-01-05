16a5148d0660a17629e97d2324ebec984843e56f
Dan Hackenberg <50969139+dhackenberg@users.noreply.github.com>
2023-01-05 14:07:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My Parents New Healthy Bar')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
